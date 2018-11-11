2a7527cdef2625879ce53f5faf4fb93dcc4c3f00
chiyl <xidiandaily@126.com>
2018-11-11 20:47:53 +0800

    添加备案信息

--- themes/cnblog/footer.php
+++ themes/cnblog/footer.php
@@ -7,9 +7,10 @@
 <footer id="footer" role="contentinfo">
     <div class="container">
         <div class="col-xs-12 text-center">
-            &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
-            <?php _e('由 <a href="http://www.typecho.org" target="_blank">Typecho</a> 强力驱动'); ?>.
-            Themed <a href="https://github.com/xidiandaily/typecho-themes-plugins.git" target="_blank">Tcnblog </a> edit by <a href="http://blog.chiyl.info/index.php/start-page.html" target="_blank">LawrenceChi</a>
+            <?php _e('版权所有'); ?> &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
+            <?php _e('|粤ICP备16000124号-1|虚无之地'); ?>.
+            <?php _e('|由 <a href="http://www.typecho.org" target="_blank">Typecho</a> 强力驱动 | Themed'); ?>
+            <a href="https://github.com/xidiandaily/typecho-themes-plugins.git" target="_blank">Tcnblog </a> edit by <a href="http://blog.chiyl.info/index.php/start-page.html" target="_blank">LawrenceChi</a>
         </div>
     </div>
 </footer><!-- end #footer -->
